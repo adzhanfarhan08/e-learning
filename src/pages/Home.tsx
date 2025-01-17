ea27a4d67b41521e3d76efa5fd11fad6acfdb8f2
adzhanfarhan08 <adzhanfarhan@gmail.com>
2025-01-18 03:17:47 +0700

    fix: refactoring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,26 @@
-import { Category } from '../components/Category';
+import { Card } from '../components/Card';
 
 const Home = () => {
   return (
     <div className="bg-yellow-200">
-      <main id="section" className="min-h-screen">
-        <div className="flex gap-96 justify-center items-center">
-          <div className="text-3xl font-bold">Selamat Datang di LMS</div>
-          <div>
-            <img src="/src/assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" alt="" className="w-96" />
+      <main>
+        <section className="py-40">
+          <div className="flex gap-96 justify-center items-center">
+            <div className="text-3xl font-bold">Selamat Datang di LMS</div>
+            <div>
+              <img src="/src/assets/images/alex-knight-j4uuKnN43_M-unsplash.jpg" alt="" className="w-96" />
+            </div>
           </div>
-        </div>
-        <Category />
+        </section>
+
+        <section className="py-40">
+          <div>
+            <h1 className="text-center font-bold text-3xl my-5">DAFTAR KELAS</h1>
+            <Card />
+          </div>
+        </section>
       </main>
+
       <footer></footer>
     </div>
   );
